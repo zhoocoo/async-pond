ce908fbd1cb12078467260e8d9c8961a9cfc78a4
LoganCong <473015937@qq.com>
2021-11-23 13:04:11 +0800

    11

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,16 +1,12 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import typescript from "@rollup/plugin-typescript";
-import { babel } from "@rollup/plugin-babel";
 import pkg from "./package.json";
 
 const libraryName = "async-control";
 
 export default {
   input: `src/${libraryName}.ts`, // 打包入口external: [],
-  watch: {
-    include: "src/**",
-  },
   output: [
     {
       file: pkg.main,
@@ -25,6 +21,5 @@ export default {
     typescript(), // 解析TypeScript
     commonjs(), // 将 CommonJS 转换成 ES2015 模块供 Rollup 处理
     resolve(), // 查找和打包node_modules中的第三方模块
-    babel({ babelHelpers: "bundled" }),
   ],
 };
