2e335b42a624758fba6bc869e53ff6fd56f8f1e9
zhaocongcong01 <zhaocongcong01@baijia.com>
2021-12-07 00:17:39 +0800

    fixed:push函数返回正确的promise

--- src/async-control.ts
+++ src/async-control.ts
@@ -4,30 +4,27 @@
 interface IPoolControlers {
   iteratorFn: Function;
   param: any;
+  flag: symbol;
 }
 
 interface IPromiseGroups {
   length: number;
-  finish: boolean;
+  finishNum: number;
+  pendingPromises: [Promise<any>];
 }
 export default class AsyncPoolPro {
   public poolLimit = 3;
   private promisePool = [] as unknown as Promise<any>[];
   private poolControlers = [] as IPoolControlers[];
-  private promiseAll = [] as unknown as null | Promise<any[]>;
   private poolIndex = 0;
   private whileControl = false;
-  private promiseGroups = {} as Record<symbol, any>;
+  private promiseGroups = {} as Record<symbol, IPromiseGroups>;
 
   constructor(poolLimit?: number) {
     this.promisePool = []; // 存储所有的异步任务，且当前异步任务均为pending态
     this.poolControlers = []; //异步请求的参数
     this.poolLimit = poolLimit || 3; //并发限制数量
-
-    this.promiseAll = null;
-
     this.poolIndex = 0;
-
     this.whileControl = false;
   }
 
@@ -44,14 +41,16 @@ export default class AsyncPoolPro {
     this.promiseGroups[flag] = {
       length: poolControlers.length,
       finishNum: 0,
-      fakePromise: new Promise(() => {}),
+      pendingPromises: [new Promise(() => {})],
     };
     this.asyncPool(this.promisePool.length === 0);
-    return Promise.all(this.promiseGroups[flag]["fakePromise"]);
+    return Promise.all(this.promiseGroups[flag].pendingPromises);
   }
 
   generatorPromise(poolControlers = this.poolControlers) {
-    const { iteratorFn, param } = poolControlers[this.poolIndex];
+    const { iteratorFn, param, flag } = poolControlers[this.poolIndex];
+    const group = this.promiseGroups[flag];
+    group.finishNum++;
     const p = Promise.resolve()
       .then(() => {
         return iteratorFn(param);
@@ -62,6 +61,10 @@ export default class AsyncPoolPro {
         this.poolControlers.splice(finishPromiseIndex, 1);
         this.poolIndex--;
       });
+    if (group.length === group.finishNum) {
+      group.pendingPromises.shift();
+    }
+    group.pendingPromises.push(p);
     this.promisePool.push(p); // 保存新的异步任务
     if (this.promisePool.length >= this.poolLimit) {
       return Promise.race(this.promisePool); // 等待较快的任务执行完成
@@ -79,6 +82,5 @@ export default class AsyncPoolPro {
       //while循环结束标识
       this.whileControl = true;
     }
-    this.promiseAll = Promise.all(this.promisePool);
   }
 }
